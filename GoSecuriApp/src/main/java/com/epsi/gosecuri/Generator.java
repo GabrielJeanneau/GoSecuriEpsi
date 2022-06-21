4e1032b312fc486752d496b20a9623c8db8b2ff8
MatthiasPELLACOEUR <61364573+MatthiasPELLACOEUR@users.noreply.github.com>
2022-06-21 23:52:30 +0200

    Update Generator.java

--- GoSecuriApp/src/main/java/com/epsi/gosecuri/Generator.java
+++ GoSecuriApp/src/main/java/com/epsi/gosecuri/Generator.java
@@ -60,8 +60,9 @@ public class Generator {
             //Récupération du template html
             //File htmlTemplateFile = new File(this.htmlDirPath+"template.html");
             //String htmlString = FileUtils.readFileToString(htmlTemplateFile);
-            String htmlString = Files.readString(Paths.get(this.htmlDirPath+"template.html"));
-            
+            //String htmlString = Files.readString(Paths.get(this.htmlDirPath+"template.html"));
+            Path chemin = Paths.get(this.htmlDirPath+"template.html");
+            String htmlString = Files.readString(chemin);
             //Initialisation des variables avec le contenu à ajouter
             String title = "Accueil";
             
